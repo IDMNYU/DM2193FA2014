c46cd11590eba5478c273f586322bf41d319d050
mdemaestri <melisademaestri@172-16-14-103.dynapool.nyu.edu>
2014-10-02 21:20:44 -0400

    inclass

--- week_04/demaestri/recipe_app 2/scripts/scripts.js
+++ week_04/demaestri/recipe_app 2/scripts/scripts.js
@@ -1,1 +1,21 @@
 // Coming in Week 5!
+
+alert('whaddupppppp');
+
+//When you click on the navigation 
+//The name of the ancore is applied as a class name on the HTML tag
+
+//Click event
+//Read attribute
+//Write attribute 
+
+
+
+   var navCheeseCake = document.getElementById('nav-chunky-cheesecake-brownies');
+
+     navCheeseCake.onclick = function(e){
+     // alert(‘hahahahaha no’);
+      //return false;
+      console.log(e);
+       };
+
