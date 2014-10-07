81ff1b3fe2254151da0c9367222b5b72b0af4d30
Kpark2993 <kup204@nyu.edu>
2014-10-07 19:52:38 -0400

    changes

---
+++ week_05/katpark/scripts/scripts.js
@@ -0,0 +1,12 @@
+
+
+var navTruffleRisotto = document.getElementById('nav-truffle-risotto');
+var navLobsterMacandCheese = document.getElementById('nav-lobster-mac-and-cheese');
+navTruffleRisotto.onclick = function(e){
+   
+    document.body.classList.add(newId);
+};
+
+navLobsterMacandCheese.onclick = function(e){
+    // console.log(e);
+};
