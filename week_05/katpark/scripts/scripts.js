0e990eada0a21df507cd3f23cd1b68231148b5f2
Kpark2993 <kup204@nyu.edu>
2014-10-07 20:50:37 -0400

    showandtellandchanges

--- week_05/katpark/scripts/scripts.js
+++ week_05/katpark/scripts/scripts.js
@@ -1,12 +1,44 @@
+// Coming in Week 5!
+
+// alert('lol');
 
 
-var navTruffleRisotto = document.getElementById('nav-truffle-risotto');
-var navLobsterMacandCheese = document.getElementById('nav-lobster-mac-and-cheese');
-navTruffleRisotto.onclick = function(e){
-   
-    document.body.classList.add(newId);
+/* these are also comments in javascript*/
+
+
+// De's Pseudo Code
+
+// When you click on the navigation
+// The name of the anchor is applied as a class name on the HTML tag
+// 
+// Click Event
+// Read Attribute
+// Write Attribute
+// 
+// "Listen for a Click Event"
+// "Read the HREF attribute from the <A>"
+// "Write the HREF attribute value as a class to <HTML>"
+
+var navTruffleRisotto = document.getElementById('nav-chunky-cheesecake-brownies');
+var navLobsterMacandCheese = document.getElementById('nav-baked-ziti');
+var CookiesandCreamBars = document.getElementById('nav-cookies-and-cream-bars');
+
+var navOnClickHandler= function(e){
+
+	console.log(e);
+
+	var navItem = e.target;
+
+	var hasClassName = navItem.hash;
+	var newId = hasClassName.substring(1, hasClassName.length-1);
+
+	document.body.className = '';
+	document.body.classList.add(newId);
+
 };
 
-navLobsterMacandCheese.onclick = function(e){
-    // console.log(e);
-};
+navTruffleRisotto.onclick = navOnClickHandler;
+navLobsterMacandCheese = navOnClickHandler;
+navCookiesandCreamBars = navOnClickHandler; 
+
+
