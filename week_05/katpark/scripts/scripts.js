fab81dd13b6f0e50a4f57a5b09a4b3a2d77f14aa
Kpark2993 <kup204@nyu.edu>
2014-10-09 19:58:57 -0400

    a;dlkfjadsf;

--- week_05/katpark/scripts/scripts.js
+++ week_05/katpark/scripts/scripts.js
@@ -15,30 +15,76 @@
 // Read Attribute
 // Write Attribute
 // 
-// "Listen for a Click Event"
+// "Listen for a Click Event on .nav-item"
 // "Read the HREF attribute from the <A>"
-// "Write the HREF attribute value as a class to <HTML>"
+// "Write the HREF attribute value as a class name to <HTML> or <BODY>"
 
-var navTruffleRisotto = document.getElementById('nav-chunky-cheesecake-brownies');
-var navLobsterMacandCheese = document.getElementById('nav-baked-ziti');
-var CookiesandCreamBars = document.getElementById('nav-cookies-and-cream-bars');
+// Fetch the element out of the DOM by it's Id
+var navCheeseCake = document.getElementById('nav-chunky-cheesecake-brownies');
+var navBakedZiti = document.getElementById('nav-baked-ziti');
+var navChocolateChipCookies = document.getElementById('nav-chocolate-chip-cookies');
 
-var navOnClickHandler= function(e){
-
-	console.log(e);
-
-	var navItem = e.target;
-
-	var hasClassName = navItem.hash;
-	var newId = hasClassName.substring(1, hasClassName.length-1);
-
-	document.body.className = '';
-	document.body.classList.add(newId);
-
+// Click Handler for Navigation
+var navOnClickHandler = function(e){
+    // e is an Event Object
+    // Log the Event Object into the Console
+    // console.log(e);
+    // This special thing
+    var navItem = e.target;
+    var hasClassName = navItem.hash;
+    // console.log(hasClassName);
+    // console.log(typeof(hasClassName));
+    var newId = hasClassName.substring(1, hasClassName.length);
+    // console.log(newId);
+    // Add the newId to the classList of Body
+    // Applying statefulness to the <BODY> of the Page
+    document.body.className = '';
+    document.body.classList.add(newId);
 };
 
-navTruffleRisotto.onclick = navOnClickHandler;
-navLobsterMacandCheese = navOnClickHandler;
-navCookiesandCreamBars = navOnClickHandler; 
+// Pseudo Code Language
+// Listen for Click Event on Navigation
+// Apply a .current Class to the navigation clicked on
+// Remove any .current classes from any other navigation
+// Click Handler for Navigation
+var navOnClickHandler2 = function(e){
+
+    // Grab the Nav Item you clicked on
+    var navItem = e.target;
+
+    // Grab all of the nav items and loop over them
+    // document.querySelectorAll() is similar to document.getElementById()
+    // except that it uses CSS selectors and it will match and return 
+    // multiple DOM elements as results in an array
+    var navItems = document.querySelectorAll('.nav-item');
+    for (var i = navItems.length - 1; i >= 0; i--) {
+        // Remove the Current Class Name
+        navItems[i].classList.remove('current');
+    }
+
+    // Re-apply current class to the nav item you clicked
+    navItem.classList.add('current');
+};
+
+// Last restort syntax
+// navCheeseCake.onclick = navOnClickHandler2;
+// navBakedZiti.onclick =  navOnClickHandler2;
+// navChocolateChipCookies.onclick =  navOnClickHandler2;
+
+// navCheeseCake.addEventListener('click', function(e){
+//     navOnClickHandler(e);
+//     navOnClickHandler2(e);
+//     // ...
+//     // Add as many methods in here as needed
+// });
+
+navCheeseCake.addEventListener('click', navOnClickHandler);
+navCheeseCake.addEventListener('click', navOnClickHandler2);
+
+navBakedZiti.addEventListener('click', navOnClickHandler);
+navBakedZiti.addEventListener('click', navOnClickHandler2);
+
+navChocolateChipCookies.addEventListener('click', navOnClickHandler);
+navChocolateChipCookies.addEventListener('click', navOnClickHandler2);
 
 
