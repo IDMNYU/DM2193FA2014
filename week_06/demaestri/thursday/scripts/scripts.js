2ab01b232f7493a7a98c29b539e7adfb480fd410
mdemaestri <melisademaestri@172-16-12-176.dynapool.nyu.edu>
2014-10-09 21:09:09 -0400

    DOM manipulation

--- week_06/demaestri/thursday/scripts/scripts.js
+++ week_06/demaestri/thursday/scripts/scripts.js
@@ -1,36 +1,65 @@
+// JS Coming Soon!
 
-// Fetch all things with the class .item
-// Assign a Click event to the items 
-$('.item').on('click', function(){
-    alert($(this).text());
-});
+// Fetch all things with the .item
+// Assign a Click event to the items
+$('.item').on('mouseenter', function(){
 
-//OTHER FUNCTIONS: 
-//alert('some text');
+     var $item = $(this);
 
-// "Give me the thing I just clicked"
-// $(this)
+    $item.parent().animate({
+         'padding': '100px' 
+     }, 'slow');
 
-// console.log($(this).text());
+    // alert('some text');
 
-//console.log('MOUSE ENTER', $.trim($(this).text())); 
+    // "Give me the thing I just clicked"
+    // $(this)
 
-//Get the Text contents out of the DOM element
-//$(this).text();
+    // console.log($(this).text());
+    // console.log('MOUSE ENTER', $.trim($item.text()));
 
-//Get the HTML contents out of the DOM element
-//$(this).html();
+    // Get the Text contents out of the DOM element
+    // $item.text();
 
-// jQuery best practices
-// Always assign $(this) to a variable
-// "Caching"
+    // Get the HTML contents out of the DOM element
+    // $item.html();
+
+    // jQuery best practices
+    // Always assign $(this) to a variable
+    // "Caching"
+
 
 }).on('mouseleave', function(){
+
     console.log('MOUSE LEAVE', $.trim($(this).text()));
+
+    var $item = $(this);
+
+    $item.parent().animate({
+         'padding': '100px' 
+     }, 'slow');
+
 }).on('click', function(){
 
     var $item = $(this);
 
+    // $item.addClass('collapse');
+    // $item.removeClass('collapse');
     $item.toggleClass('collapse');
 
 });
+
+// mousedown
+// mouseup
+// mouseenter
+// mouseleave
+
+// dragstart
+// dragstop
+
+//GENERATIVE ART 
+//$(window).on('mousemove', function(){
+  //  var container_html = $('#container').html();
+    //$('body').append(container_html);
+//});
+
