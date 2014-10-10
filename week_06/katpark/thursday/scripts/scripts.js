eac157848ddc76083e807706910e161597c3f663
Kpark2993 <kup204@nyu.edu>
2014-10-09 21:12:37 -0400

    class

--- week_06/katpark/thursday/scripts/scripts.js
+++ week_06/katpark/thursday/scripts/scripts.js
@@ -1,1 +1,16 @@
 // JS Coming Soon!
+
+
+$('#item_1').on('click', function(){
+	alert('Katalina Park is overlord, master of all, doer of all good. A BOSS~(*.*)~');
+});
+
+//the dollar sign is saying go and get element by anything 
+
+//(this) applies to the item that you are clicking on 
+
+$(window).on('mousemove', function(){
+	var container_html =$('#container_1').html;
+	$('body').append(container_html);
+
+}); 
