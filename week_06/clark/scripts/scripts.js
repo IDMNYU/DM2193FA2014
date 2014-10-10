1f50b64091af98fea98e4aa2eea567ff29c749b7
meggc94 <mclark414@gmail.com>
2014-10-09 21:08:52 -0400

    All Class Work done for Week 6

--- week_06/clark/scripts/scripts.js
+++ week_06/clark/scripts/scripts.js
@@ -1,29 +1,64 @@
-// Coming in Week 5!
-//alert("hi");
+// 
+// Fetch all things with the .item
+// Assign a Click event to the items
 
-/*
-	Pseudo Code
+$('.item').on('mouseenter', function(){
 
-*/
-//When you click on the navigation
-//The name of the anchor is applied as a class name on the HTML tag
+    var $item = $(this);
 
-// Click Event
-// Read Attribute
-// Write Attribute
+    $item.parent().animate({
+    	'padding': '20px',
+    }, 'slow')
 
-//"Listen for a Click Event"
-//"Read the HREF attribut from the <A>"
-//"Write the HREF attribute value as a class to <HTML>"
+    // alert('some text');
 
-var navCheesecake = document.getElementsById('nav-chunky-cheesecake-brownies')
-var navZiti = document.getElementsById('nav-baked-ziti')
-var navChookies = document.getElementsById('nav-chocolate-chip-cookies')
-navCheesecake.onclick = function(e){
-	//alert("hahahahah no");
-	//return false;
-	//Show the event object
-	var hashCheeseCake = navCheesecake.hash;
-	//console.log(navCheesecake.hash);
-}
-	
+    // "Give me the thing I just clicked"
+    // $(this)
+
+    // console.log($(this).text());
+    // console.log('MOUSE ENTER', $.trim($item.text()));
+
+    // Get the Text contents out of the DOM element
+    // $item.text();
+
+    // Get the HTML contents out of the DOM element
+    // $item.html();
+
+}).on('mouseleave', function(){
+
+    //console.log('MOUSE LEAVE', $.trim($(this).text()));
+    var $item = $(this);
+    $item.parent().animate({
+    	'padding': '10px',
+    }, 'slow')
+
+}).on('click', function(){
+
+    var $item = $(this);
+
+    // $item.addClass('collapse');
+    // $item.removeClass('collapse');
+    $item.toggleClass('collapse');
+
+});
+// jQuery best practice
+// Always assign $(this) to a variable
+// "Caching"
+
+// mousedown
+// mouseup 
+// mouseenter
+// mouseleave
+
+// dragstart
+// dragstop
+
+
+$(window).on('mousemove', function(){
+	var container_html = $('#container_1').html();
+	$('body').prepend(container_html);
+});
+
+
+
+
