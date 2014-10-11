9c1758b0d618bdfe4e93d38454ea185d22dfe7da
Abdullah Sarwar <asarwar@nyu.edu>
2014-10-11 03:10:45 -0400

    one-page website now more functional

--- week_04/asarwar/scripts/scripts.js
+++ week_04/asarwar/scripts/scripts.js
@@ -1,4 +1,27 @@
 function zIndexChanger(recipeSelect){
-	var value=document.getElementById(recipeSelect).style.zIndex;
-	document.getElementById(recipeSelect).style.zIndex=value+1;
+	if (recipeSelect=='first-recipe'){
+		document.getElementById('home').style.zIndex=0;
+		document.getElementById('first-recipe').style.zIndex=1;
+		document.getElementById('second-recipe').style.zIndex=0;
+		document.getElementById('third-recipe').style.zIndex=0;
+	}
+	if (recipeSelect=='second-recipe'){
+		document.getElementById('home').style.zIndex=0;
+		document.getElementById('first-recipe').style.zIndex=0;
+		document.getElementById('second-recipe').style.zIndex=1;
+		document.getElementById('third-recipe').style.zIndex=0;
+	}
+	if (recipeSelect=='third-recipe'){
+		document.getElementById('home').style.zIndex=0;
+		document.getElementById('first-recipe').style.zIndex=0;
+		document.getElementById('second-recipe').style.zIndex=0;
+		document.getElementById('third-recipe').style.zIndex=1;
+	}
+	if (recipeSelect=='home'){
+		document.getElementById('home').style.zIndex=1;
+		document.getElementById('first-recipe').style.zIndex=0;
+		document.getElementById('second-recipe').style.zIndex=0;
+		document.getElementById('third-recipe').style.zIndex=0;
+	}
+
 }
