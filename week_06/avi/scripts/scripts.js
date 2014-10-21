5ff381c0a164adcb9ced169b7783f5a952491686
AvishaiRozen <avishairozenmusic@gmail.com>
2014-10-21 19:51:29 -0400

    Wireframes

--- week_06/avi/scripts/scripts.js
+++ week_06/avi/scripts/scripts.js
@@ -1,1 +1,28 @@
 // JS Coming Soon!
+
+$('.item').on('mouseenter(function(event) {
+
+
+var $item = $(this);
+
+$item.parent().animate({
+	'padding' : '20px'
+}, 'slow');
+
+}).on('mouseleave', function(){
+
+	var $item = $(this);
+
+	
+}
+
+
+
+
+
+
+
+$ ('#item_1').on('click',function(){
+	alert('hahahahaa whaaaa');
+});
+
