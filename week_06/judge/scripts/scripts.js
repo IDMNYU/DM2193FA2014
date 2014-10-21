421e1d22e4ad6ddea624314ac83be391ab83ce57
glaack <aj1239@nyu.edu>
2014-10-20 23:53:52 -0400

    added craigslist design photos

---
+++ week_06/judge/scripts/scripts.js
@@ -0,0 +1,64 @@
+// JS Coming Soon!
+
+// Fetch all things with the .item
+// Assign a Click event to the items
+$('.item').on('mouseenter', function(){
+
+    var $item = $(this);
+
+    $item.parent().animate({
+        'padding' : '20px'
+    }, 'slow');
+
+    // alert('some text');
+
+    // "Give me the thing I just clicked"
+    // $(this)
+
+    // console.log($(this).text());
+    // console.log('MOUSE ENTER', $.trim($item.text()));
+
+    // Get the Text contents out of the DOM element
+    // $item.text();
+
+    // Get the HTML contents out of the DOM element
+    // $item.html();
+
+    // jQuery best practices
+    // Always assign $(this) to a variable
+    // "Caching"
+
+
+}).on('mouseleave', function(){
+
+    //console.log('MOUSE LEAVE', $.trim($(this).text()));
+
+//}).on('click', function(){
+
+    var $item = $(this);
+
+    $item.parent().animate({
+        'padding' : '10px'
+    }, 'slow');
+
+    // $item.addClass('collapse');
+    // $item.removeClass('collapse');
+    //$item.toggleClass('collapse');
+
+});
+
+
+
+//$(window).on('mousemove', funciton(){
+  //  var container_html = $('#container_1').html();
+    //$('body').prepend(container_html);
+//});
+
+
+// mousedown
+// mouseup
+// mouseenter
+// mouseleave
+
+// dragstart
+// dragstop
