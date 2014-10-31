d0a6ed4c17bef6d4d022cb61e59a9fd63f65482b
Michael Terpak <mt2575@nyu.edu>
2014-10-31 14:08:45 -0400

    added js

--- midterm/terpak/website/scripts/script.js
+++ midterm/terpak/website/scripts/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-	
+	refresh();
+
+	$(window).on('hashchange', function(e){
+		e.preventDefault();
+		refresh();
+	});
 });
+
+function refresh(){
+	$('*').removeClass('active');
+	var hash = window.location.hash;
+
+	if (hash === '') hash = window.location.hash = 'news';
+
+	$('.nav-' + hash.substring(1, hash.length)).addClass('active');
+}
