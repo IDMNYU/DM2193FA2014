e1e87fa77aef9c786c4263b03a1db1cd76ea2851
Johnny Benson <johnny@tumblr.com>
2014-11-04 21:32:48 -0500

    In Class Coding Exercises

--- week_10/judge/modal/scripts/scripts.js
+++ week_10/judge/modal/scripts/scripts.js
@@ -13,5 +13,32 @@
 //      Hide the Modal Glass
 //      Execute Arbitrary Code
 
-// $('[data-show-modal]');
-// $('[data-dismiss-modal]');
+$('[data-show-modal]').on('click', function(){
+    var $btn = $(this);
+    var modalName = $btn.data('show-modal');
+    // console.log(modalName);
+    
+    var $modal = $('[data-modal=' + modalName + ']');
+    $modal.addClass('active');
+    // [data-modal="hello-world"]
+
+    var $glass = $('[data-modal-glass]');
+    $glass.addClass('active');
+});
+
+$('[data-dismiss-modal]').on('click', function(){
+    var $btn = $(this);
+    var affirmative = Boolean($btn.data('dismiss-modal'));
+
+    var $modal = $('.active[data-modal]');
+    $modal.removeClass('active');
+
+    var $glass = $('[data-modal-glass]');
+    $glass.removeClass('active');
+
+    if (affirmative) {
+        console.log('Sure thing');
+    } else {
+        console.log('No Whatever');
+    }
+});
