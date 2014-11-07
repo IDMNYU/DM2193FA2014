d5f7043487165e25195e3637dc675eb7dbbcba95
Kpark2993 <kup204@nyu.edu>
2014-11-06 20:54:22 -0500

    in_class_2

--- week_10/katpark/scripts/scripts.js
+++ week_10/katpark/scripts/scripts.js
@@ -1,30 +1,75 @@
-$('[data-show-modal]').on('click', function(){
-    
-    var $btn = $(this);
-    var modalName = $btn.data('show-modal');
-    
-    var $modal = $('[data-modal=' + modalName + ']');
-    $modal.addClass('active');
+var setupLightbox = function($lightbox) {
 
-    var $glass = $('[data-modal-glass]');
-    $glass.addClass('active');
 
-});
+    var $lightboxControls = $lightbox.find('[data-lightbox-control]');
+    var $lightboxLabel = $lightbox.find('[data-lightbox-label');
+    var $lightboxItems= $lightbox.find('[data-lightbox-label');
 
-$('[data-dismiss-modal]').on('click', function(){
-    
-    var $btn = $(this);
-    var affirmative = Boolean($btn.data('dismiss-modal'));
-    
-    var $modal = $('.active[data-modal]');
-    $modal.removeClass('active');
+    var itemCount = $lightboxItems.length;
+    var currentIndex = 0;
+    var nextIndex = 0;
 
-    var $glass = $('[data-modal-glass]');
-    $glass.removeClass('active');
+    var $lightboxCurrentItem = $lightboxItems.eq(nextIndex);
 
-    if (affirmative) {
-        console.log('OK Cool');
-    } else {
-        console.log('Meh');
+    if (itemCount ===0) {
+        return;
+    } else if ( itemCount === 1){
+        $lightboxControls.hide();
+    } else { 
+        $lightboxControls.on('click' , function() {
+
+            var $btn = $(this);
+            currentIndex = $lightbox.find('.active[data-lightbox-item]').index();
+       
+
+             if ($btn.data('lightbox-control') === 'previous') 
+
+             }
+        
+            if ($btn.data('lightbox-control') === 'next') {
+            next = currentIndex-1; 
+
+             }
+    });
+
+        RlightboxItems.removeClass('active');
+
+        $lightboxCurrentItem = $lightboxItems.eq(nextIndex);
+
+        $lightboxCurrentItem.addClass('active');
+
     }
+
+    $lightbox.addClass('active');
+
+    var $glass = $('[data-lightboxy-glass]');
+    $glass.addClass('active');
+};
+
+
+
+$('[data-open-lightbox]').on('click', function() {
+    var $btn = $(this);
+    console.log("yo")
+    var lightboxName = $btn.data('open-lightbox');
+    var $lightbox = $('[data-lightbox=' + lightboxName + ']');
+
+    setupLightbox($lightbox);
+
+    $lightbox.addClass('active');
+
+    var $glass = $('[data-lightboxy-glass]');
+    $glass.addClass('active');
 });
+
+$('[data-dismiss-lightbox]').on('click', function(){
+
+    var $btn = $(this)
+    var lightboxName = $btn.data('open-lightbox');
+    var $lightbox = $('[data-lightbox=' + lightboxName + ']');
+    $lightbox.addClass('active');
+
+    var $glass = $('[data-lightboxy-glass]');
+    $glass.removeClass('active');
+   
+    {);
