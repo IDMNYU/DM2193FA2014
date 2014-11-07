7d091779fda52b8072426f22827b0f98b012ca22
Kpark2993 <kup204@nyu.edu>
2014-11-06 21:19:05 -0500

    inclass

--- week_10/katpark/scripts/scripts.js
+++ week_10/katpark/scripts/scripts.js
@@ -23,20 +23,27 @@ var setupLightbox = function($lightbox) {
        
 
              if ($btn.data('lightbox-control') === 'previous') 
+                nextIndex = (currentIndex === 0) ? itemCount-1; currentIndex-1;
 
              }
         
             if ($btn.data('lightbox-control') === 'next') {
-            next = currentIndex-1; 
+                nextIndex = (currentIndex === itemCount-1) ? 0 : currentIndex+
 
              }
     });
 
-        RlightboxItems.removeClass('active');
+        $lightboxItems.removeClass('active');
 
         $lightboxCurrentItem = $lightboxItems.eq(nextIndex);
 
         $lightboxCurrentItem.addClass('active');
+
+        $lightboxLabel.text(
+            
+            $lightboxCurrentItem.find('img').attr('alt')
+
+            );
 
     }
 
@@ -46,10 +53,21 @@ var setupLightbox = function($lightbox) {
     $glass.addClass('active');
 };
 
+    var destroyLightbox = function($lightbox) {
+
+        var $lightboxControls = $lightbox.find('[data-lightbox-control]');
+        $lightboxControls.off('click');
+
+        var $lightboxLabel = $lightbox.find('[data-lightbox-label]');
+        $lightboxLabel.text($lightboxLabel.data['[lightbox-label]'));
+
+        $lightbox.find('.active[data-lightbox-item]').removeClass('.active');
+    }
 
 
 $('[data-open-lightbox]').on('click', function() {
     var $btn = $(this);
+    
     console.log("yo")
     var lightboxName = $btn.data('open-lightbox');
     var $lightbox = $('[data-lightbox=' + lightboxName + ']');
@@ -71,5 +89,7 @@ $('[data-dismiss-lightbox]').on('click', function(){
 
     var $glass = $('[data-lightboxy-glass]');
     $glass.removeClass('active');
+
+
    
     {);
