671d9e61665d319d82834623ccb989579d3a824d
Alexander Perez <ap.alexanderperez@gmail.com>
2014-11-11 21:17:14 -0500

    week 11

---
+++ week_11/perez/bootstrap/scripts/scripts.js
@@ -0,0 +1,11 @@
+/*
+    Site-specific Javascript
+ */
+
+// bootstrap themes
+
+// Grab all of the tooltips
+// and apply the tooltip behavior
+
+// Manually initioanlize the Tooltip functionality
+$('[data-toggle="tooltip"]').tooltip();
