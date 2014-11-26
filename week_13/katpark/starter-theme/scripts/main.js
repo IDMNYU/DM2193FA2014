5084a3f9176f8100665a98c5265e650a4b1c3b4a
Kpark2993 <kup204@nyu.edu>
2014-11-25 21:06:08 -0500

    inclassupdate

---
+++ week_13/katpark/starter-theme/scripts/main.js
@@ -0,0 +1,31 @@
+/*
+    Theme Scripts
+
+    Revealing Module Pattern
+    https://carldanley.com/js-revealing-module-pattern/
+ */
+
+var otherFunction = function() 
+	console.log('i am the other function')
+}
+
+var init = function(options) {
+	console.log('Theme code has started', options);
+nction');
+}
+
+return {
+	init: init
+
+};
+
+}(window.jQuery));
+
+}
+
+};
+
+ThemeApp.init();
+ThemeApp.otherFunction();
+ThemeApp.pizzaCat();
+
