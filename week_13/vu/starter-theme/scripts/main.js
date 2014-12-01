0029a4a8a7640cf8c285b618e8d938c6c8ae4d36
An <anvu94@gmail.com>
2014-12-01 07:54:23 -0500

    designs and stuff

--- week_13/vu/starter-theme/scripts/main.js
+++ week_13/vu/starter-theme/scripts/main.js
@@ -4,3 +4,18 @@
     Revealing Module Pattern
     https://carldanley.com/js-revealing-module-pattern/
  */
+        var ThemeApp=(function($){
+            
+            var init = function(optons){
+                console.log('Theme code has started', options);
+            };
+            
+            return{
+                init: init
+            };
+        }(window.jQuery))
+
+        //Anonymous self executing functions
+        //
+
+        ThemeApp.init(); //this runs
